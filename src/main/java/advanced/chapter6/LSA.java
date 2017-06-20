bd328446b59e607d65ce8754468e541efa655110
jiangpeizhao <jiangpeizhao@ctvit.com.cn>
2017-06-20 17:50:59 +0800

    计算svd，下一步先解决读文件失败的bug

--- src/main/java/advanced/chapter6/LSA.java
+++ src/main/java/advanced/chapter6/LSA.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -21,8 +23,12 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.mllib.linalg.Matrix;
+import org.apache.spark.mllib.linalg.SingularValueDecomposition;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.mllib.linalg.distributed.RowMatrix;
+import org.apache.spark.rdd.RDD;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,6 +54,7 @@ public class LSA {
 		SparkConf sc = new SparkConf().setMaster("local").setAppName("Wiki LSA");
 		System.setProperty("hadoop.home.dir", "D:/Tools/hadoop-2.6.4");
 		JavaSparkContext jsc = new JavaSparkContext(sc);
+//		jsc.setLogLevel("ERROR");
 
 		//读入数据,在执行前需要先执行WriteToHadoop
 		String path = "/user/ds/wikidump.xml";
@@ -56,9 +63,8 @@ public class LSA {
 		conf.set(XMLInputFormat.END_TAG_KEY, "</page>");
 		JavaPairRDD<LongWritable, Text> kvs = jsc.newAPIHadoopFile(path, XMLInputFormat.class, LongWritable.class, Text.class, conf);
 		JavaRDD<String> rawXmls = kvs.map(p -> p.toString());
-//		JavaRDD<String> plainText = rawXmls.filter(x -> { return x != null; }).flatMap(LSA::wikiXmlToPlainText);
 //		JavaRDD<Tuple2<String, String>> plainText = rawXmls.flatMap(LSA::wikiXmlToPlainText);
-		JavaRDD<Page> plainText = rawXmls.flatMap(LSA::wikiXmlToPlainText);
+		JavaRDD<Page> plainText = rawXmls.filter(x -> { return x != null; }).flatMap(LSA::wikiXmlToPlainText);
 //		plainText.foreach(x -> System.out.println(x.tittle));
 //		TODO 读取文件存在错误
 		
@@ -66,7 +72,6 @@ public class LSA {
 		HashSet<String> stopWords = jsc.broadcast(loadStopWords("/stopwords.txt")).value();
 		JavaRDD<Lemmas> lemmatized = plainText.mapPartitions(iter -> {
 			StanfordCoreNLP pipeline = createNLPPipeline();
-//			return plainTextToLemmas(iter.next().content, stopWords, pipeline);
 			ArrayList<Lemmas> lemmasList = new ArrayList<Lemmas>();
 			lemmasList.add(new Lemmas(iter.next().tittle,plainTextToLemmas(iter.next().content, stopWords, pipeline)));
 			return lemmasList;
@@ -99,9 +104,7 @@ public class LSA {
 		}).distinct().count();
 		System.out.println("词项个数：" + count);
 		
-		//TF-IDF 2.1 计算文档频率(非分布式方式)
-//		docTermFreqs.aggregate(new HashMap<String, Integer>(), seqOp, combOp);
-		
+		//TF-IDF 2.1 计算文档频率(非分布式方式,使用aggregate,适合数据量较小的时候,在此略过)
 		//TF-IDF 2.2 计算文档频率(分布式方式)
 		//文档每出现一个不同的词项，程序就生成一个由词项和数字1 组成的键-值对
 		JavaPairRDD<String, Integer> docFreqs = docTermFreqs.flatMap(x -> x._2.keySet()).mapToPair(y -> {
@@ -112,7 +115,7 @@ public class LSA {
 				return i1 + i2;
 			}
 		});
-//		docFreqs.foreach(x -> System.out.println(x));
+		//docFreqs.foreach(x -> System.out.println(x));
 		//DF 前1500个词的文档频率
 		Integer numTerms = 1500;
 		JavaPairRDD<Integer, String> orderingSwap = docFreqs.mapToPair(x -> x.swap()).sortByKey(false);
@@ -125,22 +128,38 @@ public class LSA {
 		//为每个词项分配ID
 		JavaPairRDD<String, Long> termIds = idfs.keys().zipWithIndex();
 		Map<String, Long> bTermIds = jsc.broadcast(termIds).value().collectAsMap();
-//		bTermIds.foreach(x -> System.out.println(x));
+		JavaPairRDD<Long, String> termIdsSwap = termIds.mapToPair(x -> x.swap());
+		Map<Long, String> bTermIdsSwap = jsc.broadcast(termIdsSwap).value().collectAsMap();
+		//bTermIds.foreach(x -> System.out.println(x));
 
 		//TF-IDF 3.为每个文档建立一个含权重TF-IDF向量，稀疏矩阵
 		JavaRDD<Vector> vecs = docTermFreqs.map(termFreqs -> {
 			Integer docTotalTerms = termFreqs._2.values().stream().reduce((x, y) -> x + y).get();//一个文章中共有多少词
 			
-//			Iterator<Entry<Long, Double>> termScores = termFreqs._2.entrySet().stream().filter(x -> bTermIds.containsKey(x))
-//					.collect(Collectors.toMap(e -> bTermIds.get(e.getKey()),
-//							e -> bIdfs.get(e.getKey()) * termFreqs._2.get(e.getKey()) / docTotalTerms)).entrySet().iterator();
-			
-			List<Tuple2<Integer, Double>> termScores = termFreqs._2.entrySet().stream().filter(x -> bTermIds.containsKey(x)).map(e -> {
+			List<Tuple2<Integer, Double>> termScores = termFreqs._2.entrySet().stream().filter(x -> bTermIds.containsKey(x.getKey())).map(e -> {
 				return new Tuple2<Integer, Double>(bTermIds.get(e.getKey()).intValue(), bIdfs.get(e.getKey()) * termFreqs._2.get(e.getKey()) / docTotalTerms);
 			}).collect(Collectors.toList());
 			
 			return Vectors.sparse(bTermIds.size(), termScores);
 		});
+		//vecs.foreach(x -> System.out.println(x.toString()));
+		
+		//将行向量RDD 包装为RowMatrix 并调用computeSVD计算SVD
+		RDD<Vector> termDocMatrix = JavaRDD.toRDD(vecs);
+		termDocMatrix.cache();
+		RowMatrix mat = new RowMatrix(termDocMatrix);
+		int k = 1000;
+		boolean computeU = true;
+		double rCond = 1e-9;
+		SingularValueDecomposition<RowMatrix, Matrix> svd = mat.computeSVD(k, computeU, rCond);//20分钟
+		
+		//A≈USV
+		System.out.println("Singular values: " + svd.s());
+		Matrix v = svd.V();
+		double[] arr = v.toArray();
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 		
 		jsc.close();
 	}
@@ -158,7 +177,7 @@ public class LSA {
 		EnglishWikipediaPage page = new EnglishWikipediaPage();
 		WikipediaPage.readPage(page, xml);
 		ArrayList<Page> pageList = new ArrayList<Page>();
-		if (page.isEmpty()) {
+		if (page.isEmpty() || !page.isArticle() || page.isRedirect() || page.getTitle().contains("(disambiguation)")) {
 			return null;
 		} else {
 			Page pageEntity = new Page(page.getTitle(),page.getContent());
@@ -221,5 +240,69 @@ public class LSA {
 		}
 		return stopWordSet;
 	}
+	
+//	得到与最重要的概念最相关的词项
+	public static ArrayList<Tuple2<String, Double>> topTermsInTopConcepts(SingularValueDecomposition<RowMatrix, Matrix> svd, Integer numConcepts, Integer numTerms, Map<Long, String> bTermIdsSwap) {
+		Matrix v = svd.V();
+		ArrayList<Tuple2<String, Double>> topTerms = new ArrayList<Tuple2<String, Double>>();
+		double[] arr = v.toArray();
+		for (int i = 0; i < numConcepts; i++) {
+			int offs = i * v.numRows();
+			ArrayList<Tuple2<Double, Integer>> termWeights = new ArrayList<Tuple2<Double, Integer>>();
+			for (int j = 0; j < v.numRows(); j++) {
+				termWeights.add(new Tuple2<Double, Integer>(arr[offs + j], j));
+			}
+			Collections.sort(termWeights, new Comparator<Tuple2<Double, Integer>>() {
+				@Override
+				public int compare(Tuple2<Double, Integer> o1, Tuple2<Double, Integer> o2) {
+					if (o2._1-o1._1 > 0) {
+						return 1;
+					} else if (o2._1-o1._1 == 0){
+						return 0;
+					} else {
+						return -1;
+					}
+				}
+			});
+			List<Tuple2<String, Double>> topTermsList = termWeights.subList(0, numTerms).stream().map(x -> {
+				return new Tuple2<String, Double>(bTermIdsSwap.get(x._2.longValue()), x._1);
+			}).collect(Collectors.toList());
+			topTerms.addAll(topTermsList);
+		}
+		return topTerms;
+	}
+//	  def topTermsInTopConcepts(svd: SingularValueDecomposition[RowMatrix, Matrix], numConcepts: Int,
+//		      numTerms: Int, termIds: Map[Int, String]): Seq[Seq[(String, Double)]] = {
+//		    val v = svd.V
+//		    val topTerms = new ArrayBuffer[Seq[(String, Double)]]()
+//		    val arr = v.toArray
+//		    for (i <- 0 until numConcepts) {
+//		      val offs = i * v.numRows
+//		      val termWeights = arr.slice(offs, offs + v.numRows).zipWithIndex
+//		      val sorted = termWeights.sortBy(-_._1)
+//		      topTerms += sorted.take(numTerms).map{case (score, id) => (termIds(id), score)}
+//		    }
+//		    topTerms
+//		  }
+	public static ArrayList<Tuple2<String, Double>> topDocsInTopConcepts(SingularValueDecomposition<RowMatrix, Matrix> svd, Integer numConcepts, Integer numDocs, Map<Long, String> docIds) {
+		RowMatrix u = svd.U();
+		ArrayList<Tuple2<String, Double>> topDocs = new ArrayList<Tuple2<String, Double>>();
+		for (int i = 0; i < numConcepts; i++) {
+			Integer tmpI = i;
+			JavaPairRDD<Double, Long> docWeights = u.rows().toJavaRDD().map(x -> x.toArray()[tmpI]).zipWithIndex();
+			topDocs.addAll(docWeights.top(numDocs).stream().map(x -> new Tuple2<String, Double>(docIds.get(x._2), x._1)).collect(Collectors.toList()));
+		}
+		return topDocs;
+	}
+//		  def topDocsInTopConcepts(svd: SingularValueDecomposition[RowMatrix, Matrix], numConcepts: Int,
+//		      numDocs: Int, docIds: Map[Long, String]): Seq[Seq[(String, Double)]] = {
+//		    val u  = svd.U
+//		    val topDocs = new ArrayBuffer[Seq[(String, Double)]]()
+//		    for (i <- 0 until numConcepts) {
+//		      val docWeights = u.rows.map(_.toArray(i)).zipWithUniqueId
+//		      topDocs += docWeights.top(numDocs).map{case (score, id) => (docIds(id), score)}
+//		    }
+//		    topDocs
+//		  }
 }
 	
